aae95dbdb66e2e25fe127872a4164f35d14a8cf5
Morgan <morganwest955@gmail.com>
2017-12-07 12:34:46 -1000

    Added some functionality.  DOES NOT RUN

--- coordFinder.py
+++ coordFinder.py
@@ -4,6 +4,7 @@ Copyright 2017: Morgan West, Rebekah Loving, Morgan Friend
 """
 
 import math
+import utm
 
 # This class will take the x,y coordinates of fauna found in a given
 # image and output the real world coordinates.
@@ -20,7 +21,7 @@ class CoordFinder:
     # if there is no defined direction, pass direction as 0 to assume north-up orientation
     def __init__(self,FOV,direction,height):
         self.theta = float(FOV / 2)
-        self.direction = direction # polar coordinate
+        self.phi = math.radians(direction) # polar rotation coordinate converted to radians
         self.droneHeight = height # in meters
         return
 
@@ -33,15 +34,18 @@ class CoordFinder:
         longitude = droneLong
         newLatitude = float(latitude[0] + float(latitude[1] / 60) + float(latitude[2] / 3600))
         newLongitude = float(longitude[0] + float(longitude[1] / 60) + float(longitude[2] / 3600))
-        print newLatitude, newLongitude
+        ##print(newLatitude, newLongitude)
         return (newLatitude,newLongitude)
+
+    # Converts decimal degrees to universal transverse mercador in zone 5Q (Big Island).
+    # Returns a coordinate tuple in UTM.
+    def toUTM(self,coords):
+        latitude = coords[0]
+        longitude = coords[1]
+        return utm.from_latlon(latitude,longitude,5,'Q')
 
     # Takes the coordinate given from the image data and calculates the edge
     def getEdges(self,droneLat,droneLong,imageRatio):
-        # TODO: implement directional calculation
-        # TODO: convert distToEdge from meters to decimal degrees
-            # can use haversine formula for 100% accuracy (complicated), but
-            # precision of individual trees: 0.00001 DD = 1.0247m at 23 degrees N/S
         droneCoords = self.toDecimalDegrees(droneLat,droneLong)
         edges = [] # list of edges [top, right, bottom, left]
         distToEdge = float(self.droneHeight*math.tan(self.theta))
@@ -49,45 +53,34 @@ class CoordFinder:
         # 1 m = 0.0004858 dd
         distToEdge = float(distToEdge * 0.0004858) # conversion from meters to decimal degrees
         # edges assuming orientation is north-up
-        # can be changed later when direction variable is introduced
-        edges.append((droneCoords[0] + (imageRatio * distToEdge), droneCoords[1])) # N
-        edges.append((droneCoords[0], droneCoords[1] - distToEdge)) # E
-        edges.append((droneCoords[0] - (imageRatio * distToEdge), droneCoords[1])) # S
-        edges.append((droneCoords[0], droneCoords[1] + distToEdge)) # W
-        # TODO: Implement directional calculation
-            # probably do it before adding to the drone coords too
-        # TODO: convert degree (phi) direction to radians
+        # directional calculation & distance
         # x' = xcos(phi)-ysin(phi)
-        # y' = ycox(phi)+xsin(phi)
+        # y' = ycos(phi)+xsin(phi)
+        # TODO: fix the math because it ain't right
+        NSdistToEdge = distToEdge * imageRatio # since the image is wider than it is tall
+        edges.append((droneLat + 0 - (NSdistToEdge * math.sin(self.phi)), \
+                      (droneLong + NSdistToEdge * math.cos(self.phi) + 0))) # N
+        edges.append((droneLat + -distToEdge * math.cos(self.phi) - 0, \
+                      (droneLong + 0 + (-distToEdge * math.sin(self.phi))))) # E
+        edges.append((droneLat + 0 - (-NSdistToEdge * math.sin(self.phi)), \
+                      (drongLong + -NSdistToEdge * math.cos(self.phi) + 0))) # S
+        edges.append((droneLat + distToEdge * math.cos(self.phi) - 0, \
+                      (droneLong + 0 + (distToEdge * math.sin(self.phi))))) # W
+        for i in range(edges):
+            edge[i] = self.toUTM(edge[i])        
         return edges
+
+    # Finds the midpoint of the coordinates calculated for multiple images that contain the same
+        # geographic location
+    # TODO: this funciton
+    def triangulate(self,coordinates):
+
+        return
     
-    # imageResolution should be a tuple (eg (4000,4000)) with the format (width,height)
-    # droneHeight is an integer, should be available in image metadata
-    # droneCoords should be in the image metadata, should be passed as a tuple
-    # direction is not available in the image metadata but should be passed as a polar
-    # coordinate.  If it is not available, pass it as 0
-    # plantCoords should be an array of tuples for the coordinates in the image
+    # Function for processing real life UTM coordinates given image and image coordinates of
+        # detected plants
     def processCoords(self,droneLat,droneLong,plantCoords,imageX,imageY):
         imageRatio = float(float(imageX) / float(imageY))
         edges = self.getEdges(droneLat,droneLong,imageRatio)
         return edges
-##        ratioList = [] # list of tuples that represent the ratio which coordinates are from the center to the edge
-##        for x,y in plantCoords:
-##            if x == 0 and y == 0: # in center
-##                
-##            if x == 0 and y >= 0: # center positive y axis
-##
-##            if x == 0 and y <= 0: # center negative y axis
-##
-##            if x >= 0 and y == 0: # center positive x axis
-##
-##            if x <= 0 and y == 0: # center negative x axis
-##
-##            if x > 0 and y > 0: # quadrant 1 (x,y)
-##
-##            if x < 0 and y > 0: # quadrant 2 (-x,y)
-##
-##            if x < 0 and y < 0: # quadrant 3 (-x,-y)
-##
-##            if x > 0 and y < 0: # quadrant 4 (x,-y)
                 
