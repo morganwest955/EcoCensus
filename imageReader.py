5dd090da4458dc398819c3984c6e60bbcedbaf5e
Morgan <morganwest955@gmail.com>
2018-03-30 12:44:15 -1000

    Changed everything (see notes)
    imageReader is now a class that returns the image dimensions, drone coordinates, and drone yaw in a tuple.  Import it into the main file and use it to get that information to feed into coordFinder.py.

--- imageReader.py
+++ imageReader.py
@@ -1,28 +1,53 @@
 """
-Image Reader
-Copyright 2017: Morgan West
+EcoCensus Image Reader
+Copyright 2018: Morgan West, Rebekah Loving
 """
 
-# TODO: Find a library to read in metadata/exifdata that puts the data in a usable format.
-#   ExifRead puts the data into an unusable format that cannot be changed or manipulated and can
-#   only be printed.
+from PIL.ExifTags import TAGS
+from PIL import Image
 
-import coordFinder as crdfdr # class to calculate coordinates of selected plants in images
+# This class is capable of reading a .jpg image and returning the following:
+#   Image dimensions, drone coordinates, and drone yaw
+# Much more information is provided in the image, however it is not relevant
+# to EcoCensus' use and therefore not extracted and returned.
+class imageReader:
 
-c = crdfdr.CoordFinder(94,30,30) # 94 FOV, 30 direction, 30 meter hight
-coords = ([19,28,53.555],[154,56,58.437])
-imageDims = (4000,3000)
-plantCoords = (1900,2136)
+    def __init__(self, image):
+        self.image = image
+        return
 
-noneType = type(None)
-coords1 = c.processCoords(coords,plantCoords,imageDims)
-coords = ([19,28,53.555],[154,56,58.437])
-if (type(coords) == noneType):
-    print("invalid input")
-plantCoords = (1900,2136)
-coords2 = c.processCoords(coords,plantCoords,imageDims)
-mid = c.midpoint((coords1[1], coords2[1]))
-print(mid)
-point = c.toUTM(mid)
-print(point)
+    # Call this function to read the defined image
+    # Returns in tuple:
+    #   Image dimensions
+    #   Drone Coordinates (geological coords)
+    #   Drone yaw (rotation relative to North) in degrees
+    def readImage():
+        # open image
+        img = Image.open(self.image)
 
+        # extracting exif information (for the coordinates)
+        exif = {
+            TAGS[k]: v
+            for k, v in img._getexif().items()
+            if k in TAGS
+        }
+
+        # size of image (dimensions)
+        size = img.size
+
+        # getting the coordinates of the drone
+        gps = exif['GPSInfo']
+        x = gps[2]
+        y = gps[4]
+        coords = (x,y)
+
+        # getting the yaw (rotation) of the drone
+        with open(self.image, "rb") as imageFile:
+          f = imageFile.read()
+          b = str(f)
+        yaw_start = b.find('FlightYawDegree=')
+        yaw_end = b.find('drone-dji:FlightPitchDegree')
+        yaw = b[yaw_start+18:yaw_end-6]
+
+        # return all relevant information
+        return(size, coords, yaw)
